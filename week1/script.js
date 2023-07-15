fe0f0be14ead8613e4d2952eacc9e5c703e9ce82
navnihalsatpute <navnihalsatpute@gmail.com>
2023-07-15 13:53:49 +0530

    week1 submission

---
+++ week1/script.js
@@ -0,0 +1,7 @@
+//Getting the buttton element by its id
+var button = document.getElementById('demobtn');
+
+//Adding a click Event Listener to the button
+button.addEventListener('click', function(){
+    alert("It worked, Congrats!");
+});
